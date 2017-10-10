f0b87de386cd5ba1939efa6ec4e32e129d9e71be
xrtShu <xrtshu@126.com>
2017-10-10 14:19:33 +0800

    更新注释

--- examples/server.js
+++ examples/server.js
@@ -4,9 +4,12 @@ const webpackDevMiddleware = require('webpack-dev-middleware')
 const webpackHotMiddleware = require('webpack-hot-middleware')
 const WebpackConfig = require('./webpack.config')
 
+// 创建express 服务
 const app = express()
 const compiler = webpack(WebpackConfig)
 
+// 挂载webpack-dev-middleware中间件
+// 提供webpack编译打包后的产品文件服务
 app.use(webpackDevMiddleware(compiler, {
   publicPath: '/__build__/',
   stats: {
@@ -15,11 +18,13 @@ app.use(webpackDevMiddleware(compiler, {
   }
 }))
 
+// 挂载热重载中间件
 app.use(webpackHotMiddleware(compiler))
 
 app.use(express.static(__dirname))
 
-const port = process.env.PORT || 8084
+// 定义端口
+const port = process.env.PORT || 8084;
 module.exports = app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}, Ctrl+C to stop`)
 })
